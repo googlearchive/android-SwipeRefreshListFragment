e80c4838cb8f64a6c0c78c1bcf9475bd08def63e
google-automerger <automerger@google.com>
2014-11-18 16:20:59 -0800

    Auto-update

--- Application/src/main/java/com/example/android/swiperefreshlistfragment/MainActivity.java
+++ Application/src/main/java/com/example/android/swiperefreshlistfragment/MainActivity.java
@@ -47,10 +47,12 @@ public class MainActivity extends SampleActivityBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        SwipeRefreshListFragmentFragment fragment = new SwipeRefreshListFragmentFragment();
-        transaction.replace(R.id.sample_content_fragment, fragment);
-        transaction.commit();
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            SwipeRefreshListFragmentFragment fragment = new SwipeRefreshListFragmentFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
     }
 
     @Override
